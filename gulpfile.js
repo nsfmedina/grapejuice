0533999c971ee8b5809b9452c69942416cfe2970
Igor Miguel <igor.miguel@grito.cc>
2017-12-14 15:31:22 -0200

    Full refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -1,46 +1,42 @@
-const gulp = require('gulp');
-const sass = require('gulp-sass');
-const postcss = require('gulp-postcss');
-const pixrem = require('pixrem');
-const autoprefixer = require('autoprefixer');
-const concat = require('gulp-concat');
-const browserify = require('gulp-browserify');
-const babel = require('gulp-babel');
-const bs = require('browser-sync');
-const order = require('run-sequence');
+var gulp = require('gulp');
+var sass = require('gulp-sass');
+var postcss = require('gulp-postcss');
+var pixrem = require('pixrem');
+var autoprefixer = require('autoprefixer');
+var concat = require('gulp-concat');
+var browserify = require('gulp-browserify');
+var babel = require('gulp-babel');
+var bs = require('browser-sync');
+var order = require('run-sequence');
 
-const processlog = input => { console.log(`-=-=-=-=-=-=-=-=-=-=-=---> ${input}`) }
-
-gulp.task('styles', () => {
-    processlog('styles going!');
+gulp.task('styles', function(){
     return gulp.src('src/scss/**/*.scss')
-        .pipe(sass().on('error', sass.logError))
+        .pipe(sass({
+            'outputStyle': 'nested'
+            }).on('error', sass.logError))
         .pipe(postcss([
             autoprefixer({
                 browsers: ['last 5 versions']
             }),
             pixrem()
-        ])).pipe(concat('app.css'))
+        ]))
         .pipe(gulp.dest('dist/css'));
 });
 
-gulp.task('es5main', () => {
-    processlog('es5? god have mercy on your soul');
+gulp.task('es5main', function(){
     return gulp.src('src/js/app.js')
-        .pipe(concat('bundle.js'))
+        .pipe(concat('app.js'))
         .pipe(gulp.dest('dist/js'))
 });
 
-gulp.task('es5vendor', () => {
-    processlog('vendoring');
+gulp.task('es5vendor', function(){
     return gulp.src('src/js/lib/**/*.js')
         .pipe(concat('vendor.js'))
         .pipe(gulp.dest('dist/js'))
 });
 
-gulp.task('scripts', () => {
-    processlog('taskrunning scripts');
-    return gulp.src('src/js/app.js')
+gulp.task('scripts', function(){
+    return gulp.src('src/js/bundle.js')
         .pipe(babel({
             presets: ['env']
         }))
@@ -49,32 +45,38 @@ gulp.task('scripts', () => {
         .pipe(gulp.dest('dist/js'))
 });
 
-gulp.task('assets', () => {
-    processlog('Moving assets!');
+gulp.task('assets', function(){
     return gulp.src('src/assets/**/*')
         .pipe(gulp.dest('dist/assets'));
 });
 
-gulp.task('bsync', () => {
-    processlog('Serving files!');
+gulp.task('html', function(){
+    return gulp.src('src/**/*.html')
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('bsync', function(){
     bs.init({
         server : {
-            baseDir: './'
+            baseDir: './dist/'
         }
     });
 
     gulp.watch('src/scss/**/*.scss', ['styles']);
     gulp.watch('src/js/**/*.js', ['scripts']);
     gulp.watch('src/assets/**/*', ['assets']);
+    gulp.watch('src/**/*.html', ['html']);
 
-    gulp.watch('dist/**/*').on('change', bs.reload);
-    gulp.watch('*.html').on('change', bs.reload);
+    gulp.watch('dist/**/*.css').on('change', function(){
+        gulp.src('dist/**/*.css')
+            .pipe(bs.stream());
+    });
+    gulp.watch('dist/**/*.js').on('change', bs.reload);
+    gulp.watch('dist/**/*.html').on('change', bs.reload);
 });
 
-gulp.task('next', order(['styles', 'scripts', 'assets'], 'bsync', function(){
-    console.log('There you go :)');
-}));
-
-gulp.task('default', order(['styles', 'es5main', 'es5vendor', 'assets'], 'bsync', function(){
-    console.log('There you went :)');
-}));
+gulp.task('default', function(){
+    order(['styles', 'es5main', 'es5vendor', 'assets', 'html'], 'bsync', function(){
+        console.log('done!');
+    });
+});
